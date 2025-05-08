4fedfd11eeabb0d512d15a51894d231f71d23968
FacundoDesign <skytesla777@gmail.com>
2025-05-08 08:50:23 -0300

    Update and rename MI-APELLIDO_TP-03.py to ARCHIRIA FACUNDO_ACT-01 -- TP-03.py

---
+++ 03 Estructuras Condicionales/ARCHIRIA FACUNDO_ACT-01 -- TP-03.py
@@ -0,0 +1,11 @@
+#Práctico 3: Estructuras condicionales
+#Actividad 1
+
+
+# Programa que verifica si el usuario es mayor de edad 
+edad = int(input("Ingrese su edad: ")) 
+
+if edad > 18: 
+  print("Es mayor de edad")
+
+#En este programa solicito la edad mediante input() y la convierto a entero. Luego verifico si es mayor a 18 con un if simple. Puede que el algoritmo sea demasiado sencillo.
