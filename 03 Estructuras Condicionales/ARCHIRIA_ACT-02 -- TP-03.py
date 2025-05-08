d98a4e23f5a679d3f59a3b9e2f7a05230d0fea2f
FacundoDesign <skytesla777@gmail.com>
2025-05-08 08:59:35 -0300

    Create ARCHIRIA_ACT-02 -- TP-03.py

---
+++ 03 Estructuras Condicionales/ARCHIRIA_ACT-02 -- TP-03.py
@@ -0,0 +1,14 @@
+#Práctico 3: Estructuras condicionales - Soluciones
+#Actividad 2
+
+
+# Programa para verificar si el usuario aprobó o no 
+nota = float(input("Ingrese su nota: ")) 
+
+if nota >= 6: 
+  print("Aprobado") 
+else: 
+  print("Desaprobado")
+
+#Acá usé un float porque las notas pueden tener decimales (como un 7.5). 
+#El if-else me permite mostrar un mensaje u otro dependiendo de la condición.
